d31434d9924a081768536f51afb49ae3dd2e02e2
pgermano <philipgermano@gmail.com>
2022-12-15 23:14:29 -0500

    cleaned up and did better search.

--- leetcode_algo_easy.js
+++ leetcode_algo_easy.js
@@ -24,36 +24,60 @@
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
 
-var search = function(nums, target) {
-    let iHalf = Math.floor(nums.length/2)
-    let half =nums[iHalf]
+// var search = function(nums, target) {
+//     let iHalf = Math.floor(nums.length/2)
+//     let half =nums[iHalf]
     
-    if(nums[0] === target){
-        return 0
-    }
-    if(half > target){
-        for(i=iHalf; i>0; i--){
-            if(nums[i] === target){
-                return i
-            }
+//     if(nums[0] === target){
+//         return 0
+//     }
+//     if(half > target){
+//         for(i=iHalf; i>0; i--){
+//             if(nums[i] === target){
+//                 return i
+//             }
            
-        }
-        return -1
-    }else if(half < target){
-            for(i=iHalf; i<nums.length; i++){
-            if(nums[i] === target){
-                return i
-            }
+//         }
+//         return -1
+//     }else if(half < target){
+//             for(i=iHalf; i<nums.length; i++){
+//             if(nums[i] === target){
+//                 return i
+//             }
             
-        }
-        return -1
-    }else if(half === target){
-        return iHalf
-    } else{
-        return -1
-    }
-};
+//         }
+//         return -1
+//     }else if(half === target){
+//         return iHalf
+//     } else{
+//         return -1
+//     }
+// };
 
-// console.log(search([-1,0,3,5,9,12],9), 'binary search algo')
-// console.log(search([-1,0,3,5,9,12],2), 'binary search algo')
+var search = function(nums, target) {
+    let min = 0;
+    let max = nums.length-1;
+    let mid = Math.floor((max-min)/2)
+ 
+    while (min<= max){
+        if(nums[mid] === target){
+            return mid
+        }else if(nums[mid]>target){
+             max = mid-1;
+             mid = Math.floor((max+min)/2)
+ 
+             console.log(min,'min', max, 'max', mid,'mid', 'mid>target')
+        }else if(nums[mid]<target){
+             min = mid+1;
+             mid = Math.floor((max+min)/2)
+             console.log(min,'min', max, 'max', mid,'mid', 'mid<targ')
+ 
+        }
+ 
+    }
+    return -1
+ };
+
+console.log(search([-1,0,3,5,9,12],9), 'binary search algo')
+console.log(search([-1,0,3,5,9,12],2), 'binary search algo')
 
