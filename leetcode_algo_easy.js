67e21b4ca92fc48eb170e0704ee5ddee20b57e56
pgermano <philipgermano@gmail.com>
2022-12-16 22:34:42 -0500

    leetcode 278 solution

--- leetcode_algo_easy.js
+++ leetcode_algo_easy.js
@@ -1,4 +1,5 @@
 //704 binary search
+//#region 
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
 // You must write an algorithm with O(log n) runtime complexity.
@@ -54,30 +55,103 @@
 //     }
 // };
 
-var search = function(nums, target) {
-    let min = 0;
-    let max = nums.length-1;
-    let mid = Math.floor((max-min)/2)
+// var search = function(nums, target) {
+//     let min = 0;
+//     let max = nums.length-1;
+   
  
-    while (min<= max){
-        if(nums[mid] === target){
-            return mid
-        }else if(nums[mid]>target){
-             max = mid-1;
-             mid = Math.floor((max+min)/2)
+//     while (min<= max){
+//         let mid = Math.floor((max+min)/2)
  
-             console.log(min,'min', max, 'max', mid,'mid', 'mid>target')
-        }else if(nums[mid]<target){
-             min = mid+1;
-             mid = Math.floor((max+min)/2)
-             console.log(min,'min', max, 'max', mid,'mid', 'mid<targ')
+//         if(nums[mid] === target){
+//             return mid
+//         }else if(nums[mid]>target){
+//              max = mid-1;
  
-        }
+//         }else if(nums[mid]<target){
+//              min = mid+1;
  
-    }
-    return -1
- };
+//         }
+ 
+//     }
+//     return -1
+//  };
+// console.log(search([-1,0,3,5,9,12],9), 'binary search algo')
+// console.log(search([-1,0,3,5,9,12],2), 'binary search algo')
 
-console.log(search([-1,0,3,5,9,12],9), 'binary search algo')
-console.log(search([-1,0,3,5,9,12],2), 'binary search algo')
+//#endregion
+
+
+//278. First Bad Version
+//#region 
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+ 
+
+// Example 1:
+
+// Input: n = 5, bad = 4
+// Output: 4
+// Explanation:
+// call isBadVersion(3) -> false
+// call isBadVersion(5) -> true
+// call isBadVersion(4) -> true
+// Then 4 is the first bad version.
+// Example 2:
+
+// Input: n = 1, bad = 1
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= bad <= n <= 231 - 1
+
+//SOLUTION
+
+// /**
+//  * Definition for isBadVersion()
+//  * 
+//  * @param {integer} version number
+//  * @return {boolean} whether the version is bad
+//  * isBadVersion = function(version) {
+//  *     ...
+//  * };
+//  */
+
+// /**
+//  * @param {function} isBadVersion()
+//  * @return {function}
+//  */
+// var solution = function(isBadVersion) {
+//     /**
+//      * @param {integer} n Total versions
+//      * @return {integer} The first bad version
+//      */
+//     return function(n) {
+//              //binary search. check mid. if bad max = mid -1 if good min = bad+1.
+//             min = 0;
+//             max = n;
+//             while(min<=max){
+//                 let mid = Math.floor((min+max)/2);
+
+//                 if(isBadVersion(mid)){
+//                     max = mid -1;
+//                 }else{
+//                     min = mid +1;    
+//                 }
+//             }
+//             return min;
+        
+//     };
+// };
+
+
+//#endregion
+
+
 
