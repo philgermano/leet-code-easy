74b2454f4d997b8fb3b313ccfba00ea1e7271356
pgermano <philipgermano@gmail.com>
2022-12-16 23:58:32 -0500

    leet code 283 solved

--- leetcode_algo_easy.js
+++ leetcode_algo_easy.js
@@ -266,3 +266,68 @@
 //would be better with .map instead of for of loop. 
 
 //#endregion
+
+
+//189. Rotate Array
+//#region 
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+ 
+
+// Follow up:
+
+// Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+// Could you do it in-place with O(1) extra space?
+
+var rotate = function(nums, k) {
+    //can pop off back and unshift to the front
+    //needed both for if it needs to keep rotating them rather than just shifting one section once.
+    if (k>nums.length){
+    for(i=k;i>0;i--){
+        
+        //remove from the back
+        let last = nums.pop()
+
+        //add to front
+        nums.unshift(last);
+
+    }
+}else{
+    let splitOff = nums.splice(nums.length - k,k );
+    //splitOff = splitOff.concat(nums);
+    nums.unshift(...splitOff);
+}
+};
+
+console.log(rotate([1,2,3,4,5,6,7], 3));
+
+//#endregion
+
+
+
+
+
