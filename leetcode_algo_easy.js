6befc72572c25ddd6bf0920c4057719d9b3a3719
pgermano <philipgermano@gmail.com>
2022-12-19 22:59:51 -0500

    #283 solved

--- leetcode_algo_easy.js
+++ leetcode_algo_easy.js
@@ -327,6 +327,44 @@
 
 //#endregion
 
+//#283. Move Zeroes
+//#region
+
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+ 
+
+// Follow up: Could you minimize the total number of operations done?
+
+
+// var moveZeroes = function(nums) {
+//     //loop through array backwards so length doesn't change and jack up your index. couldn't use map because of that.
+//         for(i=nums.length-1;i>=0;i--){
+//             if(nums[i]===0){
+//                 nums.push(nums.splice(i,1));
+//             } 
+//         }
+//     };
+
+// console.log(moveZeroes([0,1,0,3,12]))
+// console.log(moveZeroes([0]))
+//#endregion
 
 
 
