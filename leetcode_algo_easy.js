52bbc3da0d389f8921536cef98331bc66ebc283b
pgermano <philipgermano@gmail.com>
2022-12-21 23:49:51 -0500

    ran through coding a linked list and some methods for them to get a better understa nding of the data structure.

--- leetcode_algo_easy.js
+++ leetcode_algo_easy.js
@@ -513,79 +513,216 @@ var middleNode = function(head) {
     
 };
 
-//OK first thing I need to use linked lists a bit to actually understand the data structure;
-class Node {
-    constructor(val) {
-        this.value = val;
-        this.next = null;
-    }
-}
+
 
 
 //linked list dgeneric work to get a better understanding of their structure and uses.
 //#region 
-const one = new Node(1);
-one.next = new Node(2);
-one.next.next = new Node(3);
-one.next.next.next = new Node(4);
-one.next.next.next.next = new Node(5);
 
-class SingleLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
-    push(val){
-        const newNode = new Node(val);
-        if (!this.head){
-            this.head = newNode;
-            this.tail = newNode;
-        }else{
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++
-        return this
-    };
+//OK first thing I need to use linked lists a bit to actually understand the data structure;
+// class Node {
+//     constructor(val) {
+//         this.value = val;
+//         this.next = null;
+//     }
+// }
+// const one = new Node(1);
+// one.next = new Node(2);
+// one.next.next = new Node(3);
+// one.next.next.next = new Node(4);
+// one.next.next.next.next = new Node(5);
 
-    pop(){
-        //case: empty list
-        if(!this.head) return
+// class SingleLinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.tail = null;
+//         this.length = 0;
+//     }
+//     push(val){
+//         const newNode = new Node(val);
+//         if (!this.head){
+//             this.head = newNode;
+//             this.tail = newNode;
+//         }else{
+//             this.tail.next = newNode;
+//             this.tail = newNode;
+//         }
+//         this.length++
+//         return this
+//     };
 
-        //case:one node
-        if(this.length === 1){
-            this.head = null;
-            this.tail = null;
-            this.length = 0;
-            return;
-        };
+//     pop(){
+//         //case: empty list
+//         if(!this.head) return
 
-        //case many nodes
-        let current = this.head;
-        let newTail = null;
+//         //case:one node
+//         if(this.length === 1){
+//             this.head = null;
+//             this.tail = null;
+//             this.length = 0;
+//             return;
+//         };
 
-        while(current){
-            if (current.next){
-                newTail = current;
-            }
-            current = current.next;
-        }
-        const deletedNode = this.tail;
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        return deletedNode;
-    }
-}
+//         //case many nodes
+//         let current = this.head;
+//         let newTail = null;
 
-const list = new SingleLinkedList();
-list.push(1);
-console.log(list);
-list.push(2);
-console.log(list);
-list.pop();
-console.log(list);
+//         while(current){
+//             if (current.next){
+//                 newTail = current;
+//             }
+//             current = current.next;
+//         }
+//         const deletedNode = this.tail;
+//         this.tail = newTail;
+//         this.tail.next = null;
+//         this.length--;
+//         return deletedNode;
+//     };
+//     shift(){
+//         //case emoty list
+//         if(!this.head) return null;
+
+//         //case one mode
+//         if (this.length ===1){
+//             this.head = null;
+//             this.tail = null;
+//             this.length = 0;
+//             return;
+//         }
+//         //case : many nodes
+//         const currentHead = this.head;
+//         const newHead = currentHead.next;
+//         this.head = newHead;
+//         this.length--;
+
+//         return currentHead;
+//     };
+//     unshift(val){
+//         const newNode = new Node(val);
+
+//         //case empty list
+//         if(this.length ===0){
+//             this.head = newNode;
+//             this.tail = newNode;
+//             this.length = 1;
+//             return;
+//         }
+
+//         //case many nodes
+//         const currentHead = this.head;
+//         const newHead = newNode;
+//         this.head = newHead;
+//         newHead.next = currentHead;
+//         this.length++;
+//         return newHead;
+//     };
+//     get(index){
+//         //check edge cases
+//         if (index <0 || index >= this.length){
+//             return null;
+//         };
+//         let target = this.head;
+//         for(let i = 1; i<= index; i++){
+//             target = target.next;
+//         }
+//         return target;
+//     };
+//     set(index,val){
+//         const target = this.get(index);
+//         if(target){
+//             target.value = val;
+//             return true;
+//         }
+//         return false;
+//     };
+//     insert(index,val){
+//         //edge cases
+//         if(index < 0 || index > this.length){
+//             return false;
+//         };
+
+//         //insert end
+//         if(index === this.length){
+//             this.push(val);
+//             return true;
+//         };
+
+//         //insert begining
+//         if(index === 0){
+//             this.unshift(val);
+//             return true;
+//         };
+        
+//         //insert middle
+//         const newNode = new Node(val);
+//         const previous = this.get(index -1);
+//         const current = previous.next;
+//         previous.next = newNode;
+//         newNode.next = current;
+//         this.length++;
+
+//         return true;
+//     };
+
+//     remove(index){
+//         //edge cases
+//         if(index<0 || index <= this.length){
+//             return false;
+//         };
+
+//         //remove first
+//         if(index ===0){
+//             this.shift();
+//             return true;
+//         };
+
+//         //if last node
+//         if(index === this.length -1){
+//             this.pop();
+//             return true;
+//         };
+
+//         //remove in middle;
+//         const previous = this.get(index -1);
+//         const after = previous.next.next;
+//         previous.next = after;
+//         this.length--;
+//         return true;
+//     };
+
+//     reverse(){
+//         let movingNode = this.head;
+//         this.head = this.tail;
+//         this.tail = movingNode;
+
+//         let nextNode;
+//         let previousNode = null;
+//         for(let i = 0; i < this.length; i++){
+//             //temp var for next iteration
+//             nextNode = movingNode.next;
+
+//             //main logic reverse pointer
+//             movingNode.next = previousNode;
+
+//             //prep for next iteration
+//             previousNode = movingNode;
+//             movingNode = nextNode;
+//         };
+//     };
+
+
+
+// }
+
+// const list = new SingleLinkedList();
+// list.push(1);
+// console.log(list);
+// list.push(2);
+// console.log(list);
+// list.pop();
+// console.log(list);
+
 //console.log(one);
 //console.log(one.next.next);
 //#endregion
